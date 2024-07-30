d92ea410e3ce77aa8f30c1e8c3fff3336bff7220
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-07-30 23:07:27 +0530

    switch case in cpp

---
+++ Basic.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+// SWITCH STATEMENT...............
+
+/* QUESTION  TAKE DAY NO AND PRINT THE DAYS */
+
+// int main(){
+//     int day;
+//     cin>>day;
+//     switch(day){
+//         case 1:
+//             cout<<"MONDAY";
+//             break;
+//         case 2:
+//             cout<<"TUESDAY";
+//             break;
+//         case 3:
+//             cout<<"WEDNESDAY";
+//             break;
+//         case 4:
+//             cout<<"THURSDAY";
+//             break;
+//         case 5:
+//             cout<<"FRIDAY";
+//             break;          // BREAK STATEMENT HELPS TO BREAK OUT FROM
+//         case 6:             // EVERY CONDITIONAL STATEMENT
+//             cout<<"SATURDAY";
+//             break;
+//         case 7:
+//             cout<<"SUNDAY";
+//             break;
+//         default:
+//             cout<<"invalid";
+//     }
+//     return 0;
+
+// }
+
