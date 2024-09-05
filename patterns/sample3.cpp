f116677b369cd1c7a4ac82c12770293e04a6718b
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-09-05 21:05:06 +0530

    triangle pattern updated

--- patterns/sample3.cpp
+++ patterns/sample3.cpp
@@ -11,9 +11,20 @@ void revTriangl(int n){
     }
 }
 
+void numRevTriangle(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
 int main(){
     int n;
     cout<<"enter number of rows ";
     cin>>n;
     revTriangl(n);
+    numRevTriangle(n);
 }
