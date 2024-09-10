8dc39974604d7671b8dde4b315b3aeddce46de9a
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-09-10 20:08:06 +0530

    one pattern added

--- patterns/sample5.cpp
+++ patterns/sample5.cpp
@@ -15,9 +15,27 @@ void sidePyramid(int n){
     }
 }
 
+void oneZeroTriangle(int n){
+    int start;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            start = 1;
+        }
+        else{
+            start = 0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cout<<"enter num of rows ";
     cin>>n;
     sidePyramid(n);
+    oneZeroTriangle(n);
 }
