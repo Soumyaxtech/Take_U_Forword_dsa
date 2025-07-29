d8a73875a52867f86f88b95c521e9860704063ee
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-29 14:46:45 +0530

    left rotating an array upto d places optimal approach

---
+++ array/left_rotate_d_element.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+void reverse(int arr[],int start,int end){
+    while(start<=end){
+        int temp = arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+
+void left_rotate(int arr[],int n,int d){
+        d= d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+
+        cout<<"the output array will be ";
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+
+}
+
+
+
+
+
+
+int main(){
+    int n;
+    int d;
+    cout<<"enter number of element in array ";
+    cin>> n;
+    cout<<"enter the rotating term ";
+    cin>> d;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    left_rotate(arr,n,d);
+
+
+}
