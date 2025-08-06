a3491162a6abbafb96447d47d7ce2c511162cce6
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-08-06 17:57:18 +0530

    missing number using xor added

--- array/missing_number.cpp
+++ array/missing_number.cpp
@@ -35,8 +35,22 @@ int find_missing_optimal(int arr[],int n){
     return (sum-s2);
 }
 
+// more optimal .....................................
 
 
+int find_missing_xor(int arr[],int n){
+
+    int xor1 =0;
+    int xor2 =0;
+    for(int i=0;i<n-1;i++){
+        xor2 = xor2^arr[i];
+        xor1 = xor1^(i+1);
+    }
+    xor1 = xor1^n;
+
+    return (xor1^xor2);
+
+}
 
 
 
@@ -57,5 +71,9 @@ int main(){
 
     int result1 = find_missing_optimal(arr,n);
 
-    cout<<"the missing element is "<<result1;
+    cout<<"the missing element is "<<result1<<endl;
+
+    int result2 = find_missing_xor(arr,n);
+
+    cout<<"the missing element is "<<result2;
 }
